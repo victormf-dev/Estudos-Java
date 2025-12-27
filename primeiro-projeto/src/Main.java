c08900f541637b10d3b0ee7e48cd27ba5a640b7a
Victor <victormf.dev@gmail.com>
2025-12-27 15:58:48 -0300

    Versão 2 - Mais - Javanauta

--- primeiro-projeto/src/Main.java
+++ primeiro-projeto/src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Tipos Primitivos e Variáveis (int, long, double, char e boolean) + String --> Para Caracteres
-        // primeiro declara o tipo da variável e depois atribui o nome da variável
-        // Exemplo: int (inteiro) --> idade (nome da variável)
+
+        // System.out.println --> imprime coisas na tela
+
+        // Variáveis - caixa onde guarda valores --> guarda a informação até receber o destino final
+            // Para nomear as variáveis dar nomes fáceis --> Ex: nome exato do que a variável guarda
+            // Usar sempre pra nomear a variável o padrão camelCase --> Ex: salarioMensal / calculoAcaoTrabalhista
+            // Primeiro declara o tipo da variável e depois atribui o nome da variável
+            // Exemplo: int (inteiro) --> idade (nome da variável)
+            // O símbolo '' = ''  em linguagem de programação significa atribuir - atribuição
+
+        // Tipos Primitivos (tipos mais básicos):
+            // Inteiros (números): byte, short, int e long
+            // Ponto Flutuantes (números/casas depois da vírgula): float, double
+            // Caractere: char (caractere unicode) --> Só um --> Ex: F (feminino) / M (masculino)
+                // No char precisa usar aspas simples pra declarar.
+            // Booleano (boolean): Representa verdadeiro ou falso --> True or False
+            // Tipos Primitivos mais usados: (int, long, double, char e boolean) + String --> Para Caracteres
 
         String userName = " Victor "; // usado para caracteres
         String userSurname = "Marques Ferreira "; // usado para caracteres
         int idade = 33; // para número inteiros
-        long accountBalance = 1580375L;// para número muito grandes --> tem que colocar o L no final
+        long accountBalance = 1580375855L;// para número muito grandes --> tem que colocar o L no final
         double processPayment = 37500.25; // para números com pontos flutuantes --> número depois da vírgula
         char gender = 'M'; // para declarar caractere único - unicode --> M de masculino
         boolean learningJava = true; // para declarar verdadeiro ou falso

616a23636afc4117c930a351c693f2dc52545b9f
Victor <victormf.dev@gmail.com>
2026-01-26 23:15:18 -0300

    Fix: Adicionando arquivos da maratona corretamente

---
+++ devdojo-maratona/src/ViradoNoJiraya.java
@@ -0,0 +1,9 @@
+public class ViradoNoJiraya {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Olá, Mundo!");
+        System.out.println("Meu Deus, Meu Deus, eu sou um Hacker");
+        System.out.println("DevDojo Virado no Jiraya");
+
+    }
+}

1d3e66f0a2b387649543eedc0e85e6aaae70d936
Victor <victormf.dev@gmail.com>
2026-02-19 19:31:56 -0300

    Novos exercícios de lógica de programação - DevDojo

---
+++ devdojo-maratona/src/CalculadoraDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadoraDiferencaIdade {
+    public static void main(String[] args) {
+        int firstAge = 30;
+        int secondAge = 30;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("A diferença entre as idades é: " + ageDifference);
+    }
+}
